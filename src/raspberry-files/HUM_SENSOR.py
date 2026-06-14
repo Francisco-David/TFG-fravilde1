b11a07d610a883b17296f8429fd46f995d0d1f3a
Francisco-David <fd.vilaplana@gmail.com>
2026-06-14 14:45:07 +0200

    code cleanup

--- src/raspberry-files/HUM_SENSOR.py
+++ src/raspberry-files/HUM_SENSOR.py
@@ -18,7 +18,7 @@ def read_dht11_dat(DHTPIN):
     time.sleep(0.05)
     GPIO.output(DHTPIN, GPIO.LOW)
     time.sleep(0.02)
-    GPIO.setup(DHTPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(DHTPIN, GPIO.IN, GPIO.PUD_UP) # GPIO.setup(DHTPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     time.sleep(0.05)
 
     unchanged_count = 0
